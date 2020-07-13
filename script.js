30d808cfaedcb10164b76195c192a05dd59d3c39
Elizabeth Freeman <freemanelizabeth@johndeere.com>
2020-07-13 11:12:45 -0500

    deal cards function and flip cards cuntion working

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ console.log("working");
 //Put title (header), reset (button), flip card (button) and placeholders for played cards and player stack of cards on page 
 //using CSS
 
-//build deck of cards (class for cards, class for deck)
+//build deck of cards (class for card, class for deck)
 //on page load, load deck of cards and split into two piles
 class Card {
-    constructor(value, suit) {
+    constructor(suit, value) {
         this.value = value;
         this.suit = suit;
     }
 }
 
 const suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
-const value = ["Ace", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+const value = [14, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 for (let i = 0; i < suits.length; i++) {
   for (let j = 0; j < value.length; j++) {
-    console.log(`${value[j]} of ${suits[i]}`);
+    //console.log(`${value[j]} of ${suits[i]}`);
   }
 }
 
@@ -25,7 +25,9 @@ class Deck {
     constructor() {
         this.cards = [];
         this.suits = ["hearts", "spades", "diamonds", "clubs"];
-        this.values = ["Ace", "King", "Queen", "Jack", 10, 9, 8, 7, 6, 5, 4, 3, 2];
+        this.values = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+        this.player1pile = [];
+        this.player2pile = [];
     }
     buildDeck() {
         for(let i = 0; i < this.suits.length; i++) {
@@ -45,33 +47,50 @@ class Deck {
                 this.cards[randomSpot] = temporaryValue;
             }
         }
+    dealCards (){
+        for(let i = 0; i < this.cards.length / 2; i++){
+                this.player1pile.push(this.cards[i]);
+                this.player2pile.push(this.cards[this.cards.length - i - 1]);
+            }
+            //console.log(this.player1pile);
+            //console.log(this.player2pile);
+        } 
+    flipCards () {
+        let player1card = this.player1pile.shift();
+        let player2card = this.player2pile.shift();
+       console.log(player1card);
+       console.log(player2card);
+            if (player1card.value > player2card.value) {
+            console.log("player 1 wins");
+            } else if (player2card.value > player1card.value){
+            console.log("player 2 wins");
+            //} else {(player1card === player2card);
+            //flip 4 cards and run compare cards function again
+            }
     }
-
+}
+   
 let deckOfCards = new Deck();
 deckOfCards.buildDeck();
-console.log(deckOfCards.cards);
 deckOfCards.shuffle();
+deckOfCards.dealCards();
+deckOfCards.flipCards();
 
-//press deal (button) to divide cards into two piles (player1pile, player2pile)
-let player1pile = [];
-let player2pile = [];
+//addeventListener(click) logic to flip cards button, onclick: display card from each player pile (index 0)
+//let dealButton = document.querySelector('#dealButton');
 
-//split deck into two piles
-let 
-while (deckOfCards.length > 0){
-    push.player1pile
-
-}
-
-//addeventListener(click) logic to flip cards button, onclick: display random card from each player pile
-
-//capture played values (cards or clicks) so that future clicks will only display available cards
-
-//calculate winner for each flip: if player1cardvalue > player2cardvalue else player2cardvalue > player1cardvalue, add played cards to appropriate player pile
 
 //when either player card count reaches 52(?), use alert to declare a winner
+if (player1pile.length === 52) {
+alert("Player 1 is the WINNER!")
+} else if (player2pile.length === 52) {
+alert("Player 2 is the WINNER!";
+} //else start over
 
 //enable reset to clear board --> add to reset button
 function refresh() {
     window.parent.location = window.parent.location.href;
 }
+
+//SCRATCH SECTION//
+
