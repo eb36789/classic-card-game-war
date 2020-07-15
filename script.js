c848f3db8c55a45276038f43e1494ee740e1aae0
Elizabeth Freeman <freemanelizabeth@johndeere.com>
2020-07-15 11:50:47 -0500

    failed attempt to link A/K/Q/J to values, undo til back to mvp

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ class Card {
 
 const suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
 const value = [14, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
 for (let i = 0; i < suits.length; i++) {
   for (let j = 0; j < value.length; j++) {
   }
